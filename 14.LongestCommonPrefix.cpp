f8ac5c86afccb73c012c5d9770dbaeef58fb79f4
liaoifan <liaoifan8.me97@g2.nctu.edu.tw>
2020-10-28 13:57:41 +0800

    新增第14題：找字首重複最長的詞段

---
+++ 14.LongestCommonPrefix.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include <string>
+
+class Solution{
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+    }
+};
+
+int main(int argc, char *argv[]){
+	
+	Solution x;
+
+
+	vcector <string> inputDat = {"flower", "flow", "flight"};
+	x.longestCommonPrefix();
+	
+	return 0;
+}
