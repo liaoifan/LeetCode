3e6468ab0667a5c8a9691dbf4dff0bed13e9ba2f
liaoifan <liaoifan8.me97@g2.nctu.edu.tw>
2020-10-29 12:20:13 +0800

    將第20題的測試資料方式修改為R-CO建議的自動化方式

--- no.0020_ValidParenthese/isValid.cpp
+++ no.0020_ValidParenthese/isValid.cpp
@@ -17,12 +17,13 @@ class Solution {
         0) {  // early return，輸入的字元數量如果不是偶數，直接return false
       return false;
     }
-
-    cout << "input資料處理前：";  //印出處理前的資料
-    for (ptr = input.begin(); ptr < input.end(); ptr++) {
-      cout << *ptr;
-    }
-    cout << endl;
+    /*
+        cout << "input資料處理前：";  //印出處理前的資料
+        for (ptr = input.begin(); ptr < input.end(); ptr++) {
+          cout << *ptr;
+        }
+        cout << endl;
+    */
 
     input.push_back(
         'a');  //處理之前，先隨便塞一個字元進到input後面，否則ptr+2運算到最後會出問題
@@ -41,22 +42,40 @@ class Solution {
       }
     }
     input.pop_back();  //將剛剛多塞入input的最後一個字元移除
-
-    cout << "input資料處理後：";  //印出處理後的資料
-    for (ptr = input.begin(); ptr < input.end(); ptr++) {
-      cout << *ptr;
-    }
-    cout << endl;
-
+                       /*
+                           cout << "input資料處理後：";  //印出處理後的資料
+                           for (ptr = input.begin(); ptr < input.end(); ptr++) {
+                             cout << *ptr;
+                           }
+                           cout << endl;
+                       */
     return input.empty();  //如果input被清空，則代表輸入為刮號為valid(return
-                           //1)；反之則為invalid(return 0)
+                           // 1)；反之則為invalid(return 0)
   }
 };
 
-int main(int argc, char *argv[]) {
-  Solution solution;
+struct TestCaseStruct {
+  string input;
+  bool expected_output;
+};
 
-  cout << solution.isValid("((") << endl;
+int main(int argc, char *argv[]) {
+  vector<TestCaseStruct> test_cases = {{"()", true},  //輸入的測試資料和預期答案
+                                       {"()[]{}", true}, {"(]", false},
+                                       {"([)]", false},  {"{[]}", true},
+                                       {"((", false}};
+
+  int test_case_index = 0;
+  Solution solution;
+  for (auto &test_cases : test_cases) {
+    if (solution.isValid(test_cases.input) ==
+        test_cases.expected_output) {  //將回傳值和答案做比較
+      cout << "test_case[" << test_case_index << "] is pass." << endl;
+    } else {
+      cout << "test_case[" << test_case_index << "] is fail." << endl;
+    }
+    ++test_case_index;
+  }
 
   return 0;
 }
