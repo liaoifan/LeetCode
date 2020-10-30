3a8205dff42d0ed3251cd273f602619e2b396233
liaoifan <liaoifan8.me97@g2.nctu.edu.tw>
2020-10-30 12:06:42 +0800

    新增第35題

---
+++ no.0035_SearchInsertPosition/searchInsert.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int searchInsert(vector<int>& nums, int target) {
+    vector<int>::iterator ptr;
+    int i = 0;  //計算目前指到第幾個位置
+
+    for (ptr = nums.begin(); ptr != nums.end(); ptr++) {
+      if (target <= *ptr) {
+        return i;
+      }
+      i++;
+    }
+    return i;
+  }
+};
+
+int main(int argc, char* argv[]) {
+  vector<int> nums = {1, 3, 5, 6};
+  int target = 0;
+
+  Solution solution;
+
+  cout << solution.searchInsert(nums, target) << endl;
+}
